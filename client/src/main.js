6de8308c9b6c3ebbbb620fd7f432c6be96d7a151
SoominYim <soo15soo@kakao.com>
2023-08-29 14:54:09 +0900

    Client components : Chat.vue Update

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.css";
+import "vue-material/dist/theme/black-green-light.css";
+import Directives from "../plugin/directives";
 
-createApp(App).mount('#app')
+import io from "socket.io-client";
+const socket = io("http://localhost:3000");
+
+const app = createApp(App);
+
+app.use(VueMaterial);
+app.use(Directives);
+
+app.config.globalProperties.$socket = socket;
+
+app.mount("#app");
